3eb054c2219ce7ada1b8849f5e6f085323cbdeca
Connor McGrew <connormcgrew003@gmail.com>
2023-06-26 21:32:04 -0600

    tag and product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,6 +19,12 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Category, Tag]
+    })
 });
 
 // create new product
